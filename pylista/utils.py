ea97032a0e4de9d92366016a7fd20b0b1a01d9c9
Herson Oliveira <oliveira.herson@gmail.com>
2021-07-07 23:30:44 +0200

    Add sqlalchemy

--- pylista/utils.py
+++ pylista/utils.py
@@ -2,8 +2,17 @@ import os
 from pathlib import Path
 
 def create_pylista_dir(dir: str = ".pylista") -> None:
+    """
+    Create a directory to store lists on the user home dir
+    """
     path = os.path.join(Path.home(), dir)
     try:
         os.mkdir(path)
     except FileExistsError:
         pass
+
+
+def create_list_file(name: str) -> None:
+    """
+    """
+    pass
